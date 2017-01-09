19bcf22b72938987d657a47811bea4f1bd2747b0
Kendra Lyndon <kendralyndon@gmail.com>
2017-01-09 15:32:01 -0700

    add basic directory

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
 
+export class Student {
+  id: number;
+  name: string;
+}
+
+const STUDENTS: Student[] = [
+  { id: 11, name: 'Seamus Finnegan' },
+  { id: 12, name: 'Hermione Granger' },
+  { id: 13, name: 'Ron Weasley' },
+  { id: 14, name: 'Harry Potter' },
+  { id: 15, name: 'Dean Thomas' },
+  { id: 16, name: 'Draco Malfoy' },
+  { id: 17, name: 'Cedric Diggory' },
+  { id: 18, name: 'Ginny Weasley' },
+  { id: 19, name: 'Fred Weasley' },
+  { id: 20, name: 'George Weasley' }
+];
+
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  moduleId: module.id,
+  selector: 'directory',
+  templateUrl: './templates/directory.html',
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'Hogwarts Student Directory';
+  students = STUDENTS;
+  selectedStudent: Student;
+  onSelect(student: Student): void {
+    this.selectedStudent = student;
+  };
+}
